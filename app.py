b08b974473e498604f5061bfd95d3cb4bf34cf66
Ken Roelli <kenroelli0@gmail.com>
2025-09-26 12:35:55 +0200

    Separate into landing and visualization page

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ BAG_VERSICHERER = {
     1560: "Agrisano",
     1507: "AMB Assurances SA",
     32:   "Aquilana",
-    1542: "Assura-Basis SA",
-    312:  "Atupri Gesundheitsversicherung AG",
-    343:  "Avenir Krankenversicherung AG",
+    1542: "Assura",
+    312:  "Atupri",
+    343:  "Avenir",
     1322: "Birchmeier",
     290:  "CONCORDIA",
     8:    "CSS",
     881:  "EGK",
-    134:  "Einsiedler Krankenkasse",
+    134:  "Einsiedler",
     1386: "GALENOS AG",
     780:  "Glarner",
     1562: "Helsana",
     829:  "KLuG",
     376:  "KPT",
     820:  "Lumneziana",
-    360:  "Luzerner Hinterland",
-    1479: "Mutuel Assurance Maladie SA",
+    360:  "KKLH",
+    1479: "Mutuel",
     455:  "ÖKK",
     1535: "Philos",
     1401: "rhenusana",
@@ -48,8 +48,8 @@ BAG_VERSICHERER = {
 MAX_AMOUNT_AFTER_FRANCHISE = 700
 DECIMAL_AFTER_FRANCHISE = 0.10 # We need to pay 10% of costs after franchise
 
-CHILD_LVL_TO_AMOUNT = {"FRAST1": 0, "FRAST2": 100, "FRAST3": 200, "FRAST4": 300, "FRAST5": 400, "FRAST6": 500, "FRAST7": 600}
-ADULT_LVL_TO_AMOUNT = {"FRAST1": 300, "FRAST2": 500, "FRAST3": 1000, "FRAST4": 1500, "FRAST5": 2000, "FRAST6": 2500}
+CHILD_LVL_TO_AMOUNT = {"FRAST1": "0 CHF", "FRAST2": "100 CHF", "FRAST3": "200 CHF", "FRAST4": "300 CHF", "FRAST5": "400 CHF", "FRAST6": "500 CHF", "FRAST7": "600 CHF"}
+ADULT_LVL_TO_AMOUNT = {"FRAST1": "300 CHF", "FRAST2": "500 CHF", "FRAST3": "1000 CHF", "FRAST4": "1500 CHF", "FRAST5": "2000 CHF", "FRAST6": "2500 CHF"}
 
 # Load data once when app starts (outside server function)
 try:
@@ -69,29 +69,67 @@ except Exception as e:
 # Calculate count of each PLZ and append as a new column
 premium_regions_df['plz_count'] = premium_regions_df.groupby('PLZ')['PLZ'].transform('count')
 
+municipality_choices = {"": ""} | {f"{row['Kanton']}|{row['Region']}|{row['PLZ']}|{row['Ort']}|{row['Gemeinde']}": f"{row['PLZ']} {row['Ort']}" + (f" (Gemeinde {row['Gemeinde']})" if row['plz_count'] > 1 else "")
+                for _, row in premium_regions_df.iterrows()}
 
-app_ui = ui.page_fluid(
-        ui.input_numeric("birth_year", "Birth Year", value=1990, min=1910, max=2024),
-        ui.output_ui("municipality_select"),
-        ui.output_ui("franchise_select"),
-        ui.input_radio_buttons(
-            "accident_insurance",
-            "Do you need to include accident insurance?",
-            choices={"MIT-UNF": "Yes", "OHN-UNF": "No"},
-            selected="MIT-UNF"  # or "UNF-NEIN" / "UNF-JA" as default
-        ),
-        ui.input_action_button("calculate_offers", "Calculate"),
-    ui.card(
-        ui.card_header("Insurance Cost Comparison"),
-        ui.output_data_frame("insurance_table")
-    ),
-)
-
+app_ui = ui.page_fixed( 
+        ui.panel_title("Swiss Health Insurance Premium Visualizer"),
+        ui.output_ui("dynamic_page"),
+        )
+        
 def server(input, output, session):
+    page_state = reactive.Value('landing')
+    personal_details = reactive.Value({})
+
+    @render.ui
+    def dynamic_page():
+        if page_state() == 'landing':
+            return ui.card(
+                ui.input_numeric("birth_year", "Year of birth", value=2000, min=1910, max=2024),
+                ui.output_ui("municipality_select"),
+                ui.output_ui("franchise_select"),
+                ui.input_radio_buttons(
+                    "accident_insurance",
+                    "Include accident insurance?",
+                    choices={"MIT-UNF": "Yes", "OHN-UNF": "No"},
+                    selected="MIT-UNF"
+                ),
+                ui.input_action_button("calculate_offers", "Calculate Insurance Offers")
+            )
+        elif page_state() == 'results':
+            personal_details_locked = personal_details.get()
+            return ui.card(
+                ui.h4("Entered Personal Details"),
+                ui.p(f"Year of birth: {personal_details_locked['birth_year']}"),
+                ui.p(f"Municipality: {location_display()}"),
+                ui.p(f"Franchise: {franchise_display()}"),
+                ui.p(f"Accident insurance: {accident_display()}"),
+                ui.input_action_button("modify_details", "Modify"),
+                ui.card_header("Insurance Cost Comparison"),
+                ui.output_data_frame("insurance_table")
+            )
+
+
+    @reactive.effect
+    @reactive.event(input.calculate_offers)
+    def store_details():
+        # TODO: Add input checks
+        personal_details.set({
+            "birth_year": input.birth_year(),
+            "location": input.location(),
+            "franchise": input.franchise(),
+            "accident_insurance": input.accident_insurance()
+        })
+        page_state.set('results')
     
+    @reactive.effect
+    @reactive.event(input.modify_details)
+    def back_to_landing():
+        page_state.set('landing')
+
     @reactive.calc
     def is_child():
-        return input.birth_year() >= 2008
+        return personal_details.get()['birth_year'] >= 2008
 
     @reactive.calc
     def age_category():
@@ -103,52 +141,57 @@ def server(input, output, session):
             return 'AKL-ERW'
         
     @reactive.calc
-    def franch_lvl_to_franch_amount():
-        return CHILD_LVL_TO_AMOUNT[input.franchise_select()] if is_child() else ADULT_LVL_TO_AMOUNT[input.franchise_select()]
-        
+    def franchise_display():
+        franchise_lvl = personal_details.get()['franchise']
+        return CHILD_LVL_TO_AMOUNT[franchise_lvl] if is_child() else ADULT_LVL_TO_AMOUNT[franchise_lvl]
     
+    @reactive.calc
+    def location_display():
+        loc_code = personal_details.get()['location']
+        return municipality_choices[loc_code]
+    
+    @reactive.calc
+    def accident_display():
+        return "Yes" if personal_details.get()['accident_insurance'] == "MIT-UNF" else "No"
+
     @render.ui
     def municipality_select():
         return ui.input_selectize(
             "location",
             "Enter Postal Code or Municipality:",
-            choices={"": ""} | {f"{row['Kanton']}|{row['Region']}|{row['PLZ']}|{row['Ort']}|{row['Gemeinde']}": f"{row['PLZ']} {row['Ort']}" + (f" (Gemeinde {row['Gemeinde']})" if row['plz_count'] > 1 else "")
-                for _, row in premium_regions_df.iterrows()
-            }, 
+            choices=municipality_choices, 
             # By default, we want the selection to be empty. But the empty option
             # should not be shown in the dropdown list.
             options={"allowEmptyOption": True, "showEmptyOptionInDropdown": False},
             selected=""
         )
     
+    @reactive.calc
+    def age_validation():   
+        birth_year = input.birth_year()
+        return birth_year is not None and isinstance(birth_year, int) and 1900 <= birth_year <= 2025
+
     @render.ui
     def franchise_select():
-        if is_child():
-            # Children franchise options
-            franchise_options = {
-                "FRAST1": "CHF 0",
-                "FRAST2": "CHF 100", 
-                "FRAST3": "CHF 200",
-                "FRAST4": "CHF 300",
-                "FRAST5": "CHF 400",
-                "FRAST6": "CHF 500",
-                "FRAST7": "CHF 600"
-            }
-        else:
-            franchise_options = {
-                "FRAST1": "CHF 300",
-                "FRAST2": "CHF 500", 
-                "FRAST3": "CHF 1000",
-                "FRAST4": "CHF 1500",
-                "FRAST5": "CHF 2000",
-                "FRAST6": "CHF 2500"
-            }
-        
+        birth_year = input.birth_year()
+        if not age_validation():
+            return ui.div(
+                ui.tags.label("Franchise", class_="form-label"),
+                ui.tags.select(
+                    disabled=True,
+                    title="Please enter a number of 50 or higher above to enable this selection",
+                    class_="form-select",
+                    style="opacity: 0.6;"
+                ),
+                class_="form-group shiny-input-container"
+                ) 
+        franchise_options = ADULT_LVL_TO_AMOUNT if birth_year < 2008 else CHILD_LVL_TO_AMOUNT
+
         return ui.input_select(
             "franchise",
             "Franchise",
             choices=franchise_options,
-            selected="FRAST1"
+            selected=None
         )
     
 
@@ -162,7 +205,7 @@ def server(input, output, session):
                            (premiums_df['Region'] == f"PR-REG CH{region}") & 
                            (premiums_df['Altersklasse'] == age_category()) &
                            (premiums_df['Franchisestufe'] == input.franchise()) &
-                           (premiums_df['Unfalleinschluss'] == input.accident_insurance())][['Versicherung', 'Tarif', 'Prämie']]
+                           (premiums_df['Unfalleinschluss'] == input.accident_insurance())][['Versicherung', 'Tarifbezeichnung', 'Prämie']].sort_values(by='Prämie')
 
         return df
 
